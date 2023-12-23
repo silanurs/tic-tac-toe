3dde1c75b4948ccea5d1b2b44ee95329110981ac
silanurs <sarii.silanur@gmail.com>
2023-12-23 20:08:39 +0300

    create module template

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const buttons= document.querySelectorAll('.board');
-buttons.forEach(button => button.addEventListener('click', (e) => console.log(e.target.dataset.index)))
-
-const gameBoardModule = (() => {
-let gameBoard= [0, 1, 2, 3, 4, 5, 6, 7, 8];
-return {gameBoard}
-})();
-const playerX = () => {
-   
-}
-const playerO = () => {
-    
-}
+const game = (function(){
+    const gameBoard = ["","","","","","","","",""];
+    const setPlayer=()=>{}
+    const calculateWinner =()=>{
+        let winningIndexes =
+        [[0,1,2],[3,4,5],[6,7,8],
+         [0,3,6],[1,4,7],[2,5,8],
+         [0,4,8],[2,4,6]]
+         return winningIndexes
+    } 
+    return {gameBoard,setPlayer, calculateWinner}
+})()
+console.log(game.calculateWinner())
