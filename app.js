7513e7e15dbe2d33433f5c7021d1599066854bc2
silanurs <sarii.silanur@gmail.com>
2023-12-25 20:40:00 +0300

    handled game logic

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const buttons= document.querySelectorAll('.board');
 const game = (function(){
-    const gameBoard = ["","","","","","","","",""];
+    const gameBoard = ["X","X","X","","","","","",""];
     const setPlayer=()=>{}
     const calculateWinner =()=>{
         let winningIndexes =
         [[0,1,2],[3,4,5],[6,7,8],
          [0,3,6],[1,4,7],[2,5,8],
          [0,4,8],[2,4,6]]
-         return winningIndexes
+        let xIndexes = getAllIndexes(gameBoard, "X");
+        let oIndexes = getAllIndexes(gameBoard, "O")
+        if(winningIndexes.some(arr => arr.every((val, index) => val === xIndexes[index]))){
+            return "X Won!"
+        }
+        else if (winningIndexes.some(arr => arr.every((val, index) => val === oIndexes[index]))){
+            return "O Won!"
+        }
+         
     } 
+    const getAllIndexes =(arr, val)=>{
+        let indexes =[];
+        for (let i=0; i <arr.length ;i++){
+            if(arr[i]==val){
+                indexes.push(i);   
+            }
+        }
+        return indexes;
+
+    }
     return {gameBoard,setPlayer, calculateWinner}
 })()
 console.log(game.calculateWinner())
