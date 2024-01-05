574ac66633c6576bb3b566159a67d599f18b8ce2
silanurs <sarii.silanur@gmail.com>
2024-01-05 17:18:50 +0300

    fixed bug

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const buttons= document.querySelector('.game-board');
 const playerX = document.querySelector(".player-x");
 const playerO = document.querySelector(".player-o");
-const btns = Array.from(document.querySelectorAll(".board"))
+const btns = Array.from(document.querySelectorAll(".board"));
+const msg = document.getElementById("message");
 
 const game = (function(){
     const gameBoard = [" "," "," "," "," "," "," "," "," "];
@@ -14,7 +15,9 @@ const game = (function(){
         computer="O"
         if(playerX.classList.contains("active")){
             gameBoard.splice(0,9," "," "," "," "," "," "," "," "," ");
-            btns.forEach(btn => gameBoard.map(x=>btn.textContent=x))
+            btns.forEach(btn => {
+                btn.textContent="";
+                btn.addEventListener("click",handleClick)})
         } 
         removeHandlerX();
         playerO.addEventListener("click", game.setPlayerO);
@@ -26,8 +29,12 @@ const game = (function(){
         computer="X"
         if(playerO.classList.contains("active")){
             gameBoard.splice(0,9," "," "," "," "," "," "," "," "," ");
-            btns.forEach(btn => gameBoard.map(x=>btn.textContent=x))
+            btns.forEach(btn => {
+                btn.textContent="";
+                btn.addEventListener("click",handleClick)
+            })
         } 
+        compChoice(gameBoard)
         removeHandlerO()
         playerX.addEventListener("click",game.setPlayerX)
 
@@ -46,12 +53,13 @@ const game = (function(){
         let xIndexes = getAllIndexes(gameBoard, "X");
         let oIndexes = getAllIndexes(gameBoard, "O");
         if(winningIndexes.some(arr => arr.every((val, index) => val === xIndexes[index]))){
-            return "X Won!"
+            msg.textContent= "X Won!"
         }
         else if (winningIndexes.some(arr => arr.every((val, index) => val === oIndexes[index]))){
-            return "O Won!"
-        } else {
-            return "It's a draw!"
+            msg.textContent= "O Won!"
+        } 
+        else if(!gameBoard.includes(" ")){
+            msg.textContent="It's a draw!"
         }
          
     } 
@@ -65,48 +73,73 @@ const game = (function(){
         return indexes;
 
     }
-    const compChoice = ()=>{
-        let indexes =[];
-        for (let i=0;i<gameBoard.length;i++){
-            if(gameBoard[i]===" "){
-                indexes.push(i)
+    const compChoice = (arr)=>{
+        if(player==="X"){        
+            let indexes =[];
+            for (let i=0;i<arr.length;i++){
+                if(arr[i]===" "){
+                    indexes.push(i)
+                }
             }
+            let length =indexes.length
+            let compIndex=Math.floor(Math.random()*length)
+            let x = indexes[compIndex] 
+            arr[x]=computer;  
+            let target = document.querySelector(`[data-index="${x}"]`);
+            target.textContent=computer;
+            target.classList.add("active");  
+        }else {
+            let indexes =[];
+            for (let i=0;i<arr.length;i++){
+                if(arr[i]===" "){
+                    indexes.push(i)
+                }
+            }
+            let length =indexes.length
+            let compIndex=Math.floor(Math.random()*length)
+            let x = indexes[compIndex] 
+            arr[x]=computer;  
+            let target = document.querySelector(`[data-index="${x}"]`);
+            target.textContent=computer;
+            target.classList.add("active");  
         }
-        let length =indexes.length
-        let compIndex=Math.floor(Math.random()*length)
-        
-        console.log(indexes[compIndex])
-        let x = indexes[compIndex]
-        console.log(x)
-        gameBoard.splice(x,1, computer);
-        console.log(gameBoard)
-        let target = btns[compIndex+1];
-        target.classList.add("active");
-        target.textContent=computer;
         
     }
 
+    const updateArray = (arr, inx, target)=>{
+          arr[inx]=player;
+          target.textContent=player;
+          target.classList.add("active");
+    }
     const handleClick = (e)=>{
-        
-        if(player="X"){        
-            gameBoard.splice(e.target.dataset.index,1,player)
-            e.target.textContent=gameBoard[e.target.dataset.index]
-            e.target.classList.add("active");
-            compChoice()
+        if(player==="X"){       
+             if(e.target.textContent === "" || e.target.textContent=== null){   
+            let index = e.target.dataset.index;
+            let target = e.target;
+            updateArray(gameBoard,index,target)
+            compChoice(gameBoard);
+            calculateWinner()
+            e.target.removeEventListener("click", handleClick)
+        } else {
+            e.target.removeEventListener("click", handleClick)
         }
-        else {
-            compChoice();
-            gameBoard.splice(e.target.dataset.index,1,player)
-            e.target.textContent=gameBoard[e.target.dataset.index]
-            e.target.classList.add("active");
+    } else {
+        if(e.target.textContent ==="" || e.target.textContent=== null){   
+            compChoice(gameBoard);
+            let index = e.target.dataset.index;
+            let target = e.target;
+            updateArray(gameBoard,index,target)
+            e.target.removeEventListener("click", handleClick);
+            calculateWinner()
+        } else {
+            e.target.removeEventListener("click", handleClick)
         }
-       
+
+    }
     }
    
     return {gameBoard,setPlayerX, setPlayerO, calculateWinner, handleClick}
 })()
 
-
-playerO.addEventListener("click", game.setPlayerO)
-buttons.addEventListener("click", game.handleClick)
-console.log(game.calculateWinner())
+btns.map(btn => btn.addEventListener("click", game.handleClick))
+playerO.addEventListener("click", game.setPlayerO);
